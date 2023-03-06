bef5b38ab5ea09f6c7542269f4f7ed352f6f9278
Mayer <lemaye@emea.corpdir.net>
2023-03-06 12:48:43 +0100

    Add models and metrics

---
+++ TL_Regression_Methods/BDA.py
@@ -0,0 +1,130 @@
+# encoding=utf-8
+"""
+    Created on 9:52 2018/11/14 
+    @author: Jindong Wang
+"""
+
+import numpy as np
+import scipy.io
+import scipy.linalg
+import sklearn.metrics
+import sklearn.neighbors
+from sklearn import metrics
+from sklearn import svm
+
+
+def kernel(ker, X1, X2, gamma):
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(
+                np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(
+                np.asarray(X1).T, None, gamma)
+    return K
+    
+class BDA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, mu=0.5, gamma=1, T=10, mode='BDA', estimate_mu=False):
+        '''
+        Init func
+        :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param mu: mu. Default is -1, if not specificied, it calculates using A-distance
+        :param gamma: kernel bandwidth for rbf kernel
+        :param T: iteration number
+        :param mode: 'BDA' | 'WBDA'
+        :param estimate_mu: True | False, if you want to automatically estimate mu instead of manally set it
+        '''
+        self.kernel_type = kernel_type
+        self.dim = dim
+        self.lamb = lamb
+        self.mu = mu
+        self.gamma = gamma
+        self.T = T
+        self.mode = mode
+        self.estimate_mu = estimate_mu
+
+    def fit_predict(self, Xs, Ys, Xt, Yt):
+        '''
+        Transform and Predict using 1NN as JDA paper did
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: acc, y_pred, list_acc
+        '''
+        list_acc = []
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        C = len(np.unique(Ys))
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        mu = self.mu
+        M = 0
+        Y_tar_pseudo = None
+        Xs_new = None
+        for t in range(self.T):
+            N = 0
+            M0 = e * e.T * C
+            if Y_tar_pseudo is not None and len(Y_tar_pseudo) == nt:
+                '''for c in range(1, C + 1):
+                    e = np.zeros((n, 1))
+                    Ns = len(Ys[np.where(Ys == c)])
+                    Nt = len(Y_tar_pseudo[np.where(Y_tar_pseudo == c)])
+
+                    if self.mode == 'WBDA':
+                        Ps = Ns / len(Ys)
+                        Pt = Nt / len(Y_tar_pseudo)
+                        alpha = Pt / Ps
+                        mu = 1
+                    else:
+                        alpha = 1
+
+                    tt = Ys == c
+                    e[np.where(tt == True)] = 1 / Ns
+                    yy = Y_tar_pseudo == c
+                    ind = np.where(yy == True)
+                    inds = [item + ns for item in ind]
+                    e[tuple(inds)] = -alpha / Nt
+                    e[np.isinf(e)] = 0
+                    N = N + np.dot(e, e.T)'''
+
+            # In BDA, mu can be set or automatically estimated using A-distance
+            # In WBDA, we find that setting mu=1 is enough
+            if self.estimate_mu and self.mode == 'BDA':
+                if Xs_new is not None:
+                    mu = estimate_mu(Xs_new, Ys, Xt_new, Y_tar_pseudo)
+                else:
+                    mu = 0
+            M = (1 - mu) * M0 + mu * N
+            M /= np.linalg.norm(M, 'fro')
+            K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+            n_eye = m if self.kernel_type == 'primal' else n
+            a, b = np.linalg.multi_dot(
+                [K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+            w, V = scipy.linalg.eig(a, b)
+            ind = np.argsort(w)
+            A = V[:, ind[:self.dim]]
+            Z = np.dot(A.T, K)
+            Z /= np.linalg.norm(Z, axis=0)
+            Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+
+            clf = sklearn.neighbors.KNeighborsRegressor(n_neighbors=1)
+            clf.fit(Xs_new, Ys.ravel())
+            Y_tar_pseudo = clf.predict(Xt_new)
+            acc = sklearn.metrics.mean_squared_error(Yt, Y_tar_pseudo)
+            list_acc.append(acc)
+            #print('{} iteration [{}/{}]: MSE: {:.4f}'.format(self.mode, t + 1, self.T, acc))
+        return acc, Y_tar_pseudo, list_acc
